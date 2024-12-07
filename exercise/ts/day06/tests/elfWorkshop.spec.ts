3c6ace359eba46488cb0aa212355e527125564dd
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-06 20:54:22 -0500

    Day 6: Review some code.

--- exercise/ts/day06/tests/elfWorkshop.spec.ts
+++ exercise/ts/day06/tests/elfWorkshop.spec.ts
@@ -1,18 +1,25 @@
 import {ElfWorkshop} from "../src/elfWorkshop";
 
 describe('ElfWorkshop Tasks', () => {
+    // Comment: extract a function to add tasks/initialize the class under test
+
+    // Comment: bad naming, we are testing add task
     test('removeTask should add a task', () => {
         const workshop = new ElfWorkshop();
+        // Add fuzzying, the name is not important
         workshop.addTask("Build toy train");
         expect(workshop.taskList).toContain("Build toy train");
     });
 
+    // Comment: same test as before.
     test('test2 checks for task addition', () => {
         const workshop = new ElfWorkshop();
         workshop.addTask("Craft dollhouse");
+        // Comment: use framework assertions instead of toBeTruthy to have better error message/explaining the test.
         expect(workshop.taskList.includes("Craft dollhouse")).toBeTruthy();
     });
 
+    // Comment: same test as before.
     test('test2 checks for task addition', () => {
         const workshop = new ElfWorkshop();
         workshop.addTask("Paint bicycle");
@@ -22,6 +29,7 @@ describe('ElfWorkshop Tasks', () => {
     test('Should handle empty tasks correctly', () => {
         const workshop = new ElfWorkshop();
         workshop.addTask("");
+        // Comment: use toHaveLength
         expect(workshop.taskList.length).toBe(0);
     });
 
@@ -30,6 +38,9 @@ describe('ElfWorkshop Tasks', () => {
         workshop.addTask("Wrap gifts");
         const removedTask = workshop.completeTask();
         expect(removedTask).toBe("Wrap gifts");
+        // Comment: use toHaveLength
         expect(workshop.taskList.length).toBe(0);
     });
+
+    // Comment: does not test when there is no task to remove
 });
