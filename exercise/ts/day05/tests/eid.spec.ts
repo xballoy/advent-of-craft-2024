e77ad9750a811e912c4ef0aaac629aef3c0dcb38
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-06 20:40:19 -0500

    Day 5: Validate Elf Ids (EID) (#6)
    * List test case
    
    * Invalid case null
    
    * Invalid case undefined
    
    * <type>(<scope>): <subject>
    
    <body>
    
    <footer>
    
    * Invalid case too short
    
    * Invalid case: too long
    
    * Invalid case: invalid sex
    
    * Refactor to use regex
    
    * Invalid birthdate
    
    * Check key
    
    * Add passing tests
    
    * Refactor
    
    * Add exercise 6 to CI

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -1,5 +1,63 @@
+// EID test list
+// Invalid:
+// null
+// undefined
+// ''
+// '1000010' -> too short
+// '100001000' -> too long
+// '40000100' -> invalid sex
+// '1aa00100' -> invalid birth year
+// '10000000' -> invalid serial number
+// '10000101' -> invalid key
+// Valid:
+// '19845606'
+// '30600233'
+// '29999922'
+// '11111151'
+// '19800767'
+
+import { isEid } from '../src/eid';
+
 describe('EID', () => {
-    test('implement your tests here', () => {
-        expect(43).toBe(42);
+    it('should return false when eid is null', () => {
+        expect(isEid(null)).toBe(false);
+    });
+
+    it('should return false when eid is undefined', () => {
+        expect(isEid(undefined)).toBe(false);
+    });
+
+    it('should return false when eid is empty', () => {
+        expect(isEid('')).toBe(false);
+    });
+
+    it('should return false when eid is too short', () => {
+        expect(isEid('1000010')).toBe(false);
+    });
+
+    it('should return false when eid is too long', () => {
+        expect(isEid('100001000')).toBe(false);
+    });
+
+    it('should return false when eid is an invalid sex', () => {
+        expect(isEid('40000100')).toBe(false);
+    });
+
+    it('should return false when eid is an invalid birthdate', () => {
+        expect(isEid('1aa00100')).toBe(false);
+    });
+
+    it('should return false when eid is an invalid serial number', () => {
+        expect(isEid('10000000')).toBe(false);
+    });
+
+    it.each([
+        '19845606',
+        '30600233',
+        '29999922',
+        '11111151',
+        '19800767',
+    ])('should return true when eid is valid %s', (eid) => {
+        expect(isEid(eid)).toBe(true);
     });
 });
