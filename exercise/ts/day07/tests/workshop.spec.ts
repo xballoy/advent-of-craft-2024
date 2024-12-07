58662734266de8111b2c43d61f4c191279f5fc63
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-07 15:22:05 -0500

    Day 7: Can you read and understand the tests well? (#8)

--- exercise/ts/day07/tests/workshop.spec.ts
+++ exercise/ts/day07/tests/workshop.spec.ts
@@ -2,23 +2,34 @@ import {Gift} from "../src/workshop/gift";
 import {Workshop} from "../src/workshop/workshop";
 import {Status} from "../src/workshop/status";
 
+const workshopBuilder = () => {
+    const workshop = new Workshop();
+
+    return {
+        produce: (name: string) => {
+            const gift = new Gift(name)
+            workshop.addGift(gift);
+            return {
+                status: () => gift.getStatus(),
+                complete: () => {
+                    return workshop.completeGift(name);
+                },
+            }
+        },
+        complete: (name: string) => {
+            return workshop.completeGift(name);
+        },
+    }
+}
+
 describe('Workshop', () => {
     const TOY_NAME = '1 Super Nintendo';
 
     it('completes a gift and sets its status to produced', () => {
-        const workshop = new Workshop();
-        workshop.addGift(new Gift(TOY_NAME));
-
-        const completedGift = workshop.completeGift(TOY_NAME);
-
-        expect(completedGift).not.toBeNull();
-        expect(completedGift!.getStatus()).toBe(Status.Produced);
+        expect(workshopBuilder().produce(TOY_NAME).complete().getStatus()).toBe(Status.Produced);
     });
 
     it('returns null when trying to complete a non-existing gift', () => {
-        const workshop = new Workshop();
-        const completedGift = workshop.completeGift('NonExistingToy');
-
-        expect(completedGift).toBeNull();
+        expect(workshopBuilder().complete('NonExistingToy')).toBeNull();
     });
 });
