18258e57769bb99b6587ff71537dd627b11534e5
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-09 19:01:19 -0500

    Day 9: Improve tests readability. (#10)

---
+++ exercise/ts/day09/tests/child-builder.ts
@@ -0,0 +1,44 @@
+import { GiftRequest } from '../src/giftRequest';
+import { faker } from '@faker-js/faker';
+import { Child } from '../src/child';
+import { GiftRequestBuilder } from './giftRequest-builder';
+
+export class ChildBuilder {
+    private readonly firstName: string;
+    private readonly lastName: string;
+    private readonly age: number;
+    private behavior: Behavior;
+    private giftRequest: GiftRequest;
+
+    public constructor() {
+        this.firstName = faker.person.firstName();
+        this.lastName = faker.person.lastName();
+        this.age = faker.number.int({max: 12});
+        this.behavior = 'nice';
+        this.giftRequest = new GiftRequestBuilder().withFeasibleGiftRequest().build();
+    }
+
+    withNiceBehavior(): this {
+        this.behavior = 'nice';
+        return this;
+    }
+
+    withNaughtyBehavior(): this {
+        this.behavior = 'naughty';
+        return this;
+    }
+
+    withFeasibleGiftRequest(): this {
+        this.giftRequest = new GiftRequestBuilder().withFeasibleGiftRequest().build();
+        return this;
+    }
+
+    withInfeasibleGiftRequest(): this {
+        this.giftRequest = new GiftRequestBuilder().withInfeasibleGiftRequest().build();
+        return this;
+    }
+
+    public build() {
+        return new Child(this.firstName, this.lastName, this.age, this.behavior, this.giftRequest);
+    }
+}
