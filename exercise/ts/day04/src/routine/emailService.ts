1fad6c90d581420c69de1fa08791853dc2b2fa08
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-11 21:03:24 -0500

    Migrate to monorepo (#13)
    - Apply tsconfig node 22 + strictest on all tsconfig
    - Use biome

--- exercise/ts/day04/src/routine/emailService.ts
+++ exercise/ts/day04/src/routine/emailService.ts
@@ -1,3 +1,3 @@
 export interface EmailService {
-    readNewEmails(): void;
+  readNewEmails(): void;
 }
--- exercise/ts/day04/src/routine/reindeerFeeder.ts
+++ exercise/ts/day04/src/routine/reindeerFeeder.ts
@@ -1,3 +1,3 @@
 export interface ReindeerFeeder {
-    feedReindeers(): void;
+  feedReindeers(): void;
 }
