5e0e260763b5555afeb2fa897f3f7fe8d0a7d848
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-15 17:25:34 -0500

    Day 13: Refactor your code with the Mikado method. (#15)
    * Format + fix tsconfig
    
    * Extract method to handle X for Y
    
    * Implement 2 for 1 + add test
    
    * Use option for discount
    
    * Extract method for XForAmountOffers
    
    * <type>(<scope>): <subject>
    
    <body>
    
    <footer>
    
    * Extract method for PercentageDiscountOffer
    
    * Use pattern matching for offers
    
    * Lift logic to offers
    
    * Move logic to offer

--- exercise/ts/day13/src/santamarket.model/ChristmasElf.ts
+++ exercise/ts/day13/src/santamarket.model/ChristmasElf.ts
@@ -1,35 +1,39 @@
-import { SantamarketCatalog } from './SantamarketCatalog';
-import { Product } from './Product';
 import { Offer } from './Offer';
-import { SpecialOfferType } from './SpecialOfferType';
-import { ShoppingSleigh } from './ShoppingSleigh';
+import type { Product } from './Product';
 import { Receipt } from './Receipt';
+import type { SantamarketCatalog } from './SantamarketCatalog';
+import type { ShoppingSleigh } from './ShoppingSleigh';
+import type { SpecialOfferType } from './SpecialOfferType';
 
 export class ChristmasElf {
-    private readonly catalog: SantamarketCatalog;
-    private readonly offers: Map<Product, Offer>;
+  private readonly catalog: SantamarketCatalog;
+  private readonly offers: Map<Product, Offer>;
 
-    constructor(catalog: SantamarketCatalog) {
-        this.catalog = catalog;
-        this.offers = new Map<Product, Offer>();
+  constructor(catalog: SantamarketCatalog) {
+    this.catalog = catalog;
+    this.offers = new Map<Product, Offer>();
+  }
+
+  addSpecialOffer(
+    offerType: SpecialOfferType,
+    product: Product,
+    argument: number,
+  ): void {
+    this.offers.set(product, new Offer(offerType, product, argument));
+  }
+
+  checksOutArticlesFrom(sleigh: ShoppingSleigh): Receipt {
+    const receipt = new Receipt();
+    const productQuantities = sleigh.getItems();
+
+    for (const { product, quantity } of productQuantities) {
+      const unitPrice = this.catalog.getUnitPrice(product);
+      const totalPrice = quantity * unitPrice;
+      receipt.addProduct(product, quantity, unitPrice, totalPrice);
     }
 
-    addSpecialOffer(offerType: SpecialOfferType, product: Product, argument: number): void {
-        this.offers.set(product, new Offer(offerType, product, argument));
-    }
+    sleigh.handleOffers(receipt, this.offers, this.catalog);
 
-    checksOutArticlesFrom(sleigh: ShoppingSleigh): Receipt {
-        const receipt = new Receipt();
-        const productQuantities = sleigh.getItems();
-
-        productQuantities.forEach(({ product, quantity }) => {
-            const unitPrice = this.catalog.getUnitPrice(product);
-            const totalPrice = quantity * unitPrice;
-            receipt.addProduct(product, quantity, unitPrice, totalPrice);
-        });
-
-        sleigh.handleOffers(receipt, this.offers, this.catalog);
-
-        return receipt;
-    }
+    return receipt;
+  }
 }
--- exercise/ts/day13/src/santamarket.model/ProductUnit.ts
+++ exercise/ts/day13/src/santamarket.model/ProductUnit.ts
@@ -1,4 +1,4 @@
 export enum ProductUnit {
-    KILO = 'KILO',
-    EACH = 'EACH',
+  KILO = 'KILO',
+  EACH = 'EACH',
 }
