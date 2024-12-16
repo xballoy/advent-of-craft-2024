0c088e9991e7360ee1ea2951b70a0d4a6dca0a25
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-16 15:58:45 -0500

    Day 15: Reason to fail? - Improvements (#18)
    * Repo return Either instead of nullable
    
    * Sleigh returns Either to explicit errors
    
    * Refactor business

--- exercise/ts/day15/src/dependencies.ts
+++ exercise/ts/day15/src/dependencies.ts
@@ -1,19 +1,45 @@
+import { Either, pipe } from 'effect';
+import { NonDeliverableGift } from './NonDeliverableGift';
 import type { Child, Gift, ManufacturedGift } from './models';
 
 export class Factory extends Map<Gift, ManufacturedGift> {
-  findManufacturedGift(gift: Gift): ManufacturedGift | undefined {
-    return this.get(gift);
+  findManufacturedGift(
+    gift: Gift,
+  ): Either.Either<ManufacturedGift, NonDeliverableGift> {
+    return pipe(
+      this.get(gift),
+      Either.liftPredicate(
+        (it) => !!it,
+        () => new NonDeliverableGift("Missing gift: Gift wasn't manufactured!"),
+      ),
+    );
   }
 }
 
 export class Inventory extends Map<string, Gift> {
-  pickUpGift(barCode: string): Gift | undefined {
-    return this.get(barCode);
+  pickUpGift(barCode: string): Either.Either<Gift, NonDeliverableGift> {
+    return pipe(
+      this.get(barCode),
+      Either.liftPredicate(
+        (it) => !!it,
+        () =>
+          new NonDeliverableGift(
+            'Missing gift: The gift has probably been misplaced by the elves!',
+          ),
+      ),
+    );
   }
 }
 
 export class WishList extends Map<Child, Gift> {
-  identifyGift(child: Child): Gift | undefined {
-    return this.get(child);
+  identifyGift(child: Child): Either.Either<Gift, NonDeliverableGift> {
+    return pipe(
+      this.get(child),
+      Either.liftPredicate(
+        (it) => !!it,
+        () =>
+          new NonDeliverableGift("Missing gift: Child wasn't nice this year!"),
+      ),
+    );
   }
 }
