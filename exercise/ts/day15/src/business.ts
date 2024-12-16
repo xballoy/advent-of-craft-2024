3558b0abb57002c5b2727686e161b22a312ced11
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-16 15:24:55 -0500

    Full FP with Either (#17)

--- exercise/ts/day15/src/business.ts
+++ exercise/ts/day15/src/business.ts
@@ -1,3 +1,4 @@
+import { Either, pipe } from 'effect';
 import type { Factory, Inventory, WishList } from './dependencies';
 import { type Child, Sleigh } from './models';
 
@@ -11,17 +12,40 @@ export class Business {
   loadGiftsInSleigh(...children: Child[]): Sleigh {
     const sleigh = new Sleigh();
     for (const child of children) {
-      const gift = this.wishList.identifyGift(child);
-      if (gift) {
-        const manufacturedGift = this.factory.findManufacturedGift(gift);
-        if (manufacturedGift) {
-          const finalGift = this.inventory.pickUpGift(manufacturedGift.barCode);
-          if (finalGift) {
-            sleigh.set(child, `Gift: ${finalGift.name} has been loaded!`);
-          }
-        }
-      }
+      const gift = pipe(
+        this.wishList.identifyGift(child),
+        Either.liftPredicate(
+          (gift) => !!gift,
+          () => "Missing gift: Child wasn't nice this year!",
+        ),
+        Either.flatMap((gift) =>
+          pipe(
+            this.factory.findManufacturedGift(gift),
+            Either.liftPredicate(
+              (it) => !!it,
+              () => "Missing gift: Gift wasn't manufactured!",
+            ),
+          ),
+        ),
+        Either.flatMap((manufacturedGift) =>
+          pipe(
+            this.inventory.pickUpGift(manufacturedGift.barCode),
+            Either.liftPredicate(
+              (it) => !!it,
+              () =>
+                'Missing gift: The gift has probably been misplaced by the elves!',
+            ),
+          ),
+        ),
+      );
+
+      Either.match(gift, {
+        onRight: (gift) =>
+          sleigh.set(child, `Gift: ${gift.name} has been loaded!`),
+        onLeft: (error) => sleigh.set(child, error),
+      });
     }
+
     return sleigh;
   }
 }
