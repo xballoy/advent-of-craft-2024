c75a58433db2c9eedacf50a32ffd349eaef79d07
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-19 14:44:36 -0500

    Prepare day18 and day19 (#21)

--- exercise/ts/day19/src/travel/santaTravelCalculator.ts
+++ exercise/ts/day19/src/travel/santaTravelCalculator.ts
@@ -1,17 +1,27 @@
 export class OverflowException extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = "CustomOverflowException";
-    }
+  constructor(message: string) {
+    super(message);
+    this.name = 'CustomOverflowException';
+  }
 }
-
+// biome-ignore lint/complexity/noStaticOnlyClass: keep exercise as-is
 export class SantaTravelCalculator {
-    public static calculateTotalDistanceRecursively(numberOfReindeers: number): number {
-        if(numberOfReindeers >= 32)
-            throw new OverflowException(`Overflow for ${numberOfReindeers} reindeers`);
+  public static calculateTotalDistanceRecursively(
+    numberOfReindeers: number,
+  ): number {
+    if (numberOfReindeers >= 32)
+      throw new OverflowException(
+        `Overflow for ${numberOfReindeers} reindeers`,
+      );
 
-        if (numberOfReindeers === 1) return 1;
+    if (numberOfReindeers === 1) return 1;
 
-        return 2 * this.calculateTotalDistanceRecursively(numberOfReindeers - 1) + 1;
-    }
+    return (
+      2 *
+        SantaTravelCalculator.calculateTotalDistanceRecursively(
+          numberOfReindeers - 1,
+        ) +
+      1
+    );
+  }
 }
