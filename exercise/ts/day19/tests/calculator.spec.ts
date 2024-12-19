271285417bb27763da020409264a6f345a9170c6
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-19 16:29:41 -0500

    Day 19: Performance... (#23)
    * Replace Jest by Vitest and add benchmark
    
    * Add non recursive algo
    
    * Add new test

--- exercise/ts/day19/tests/calculator.spec.ts
+++ exercise/ts/day19/tests/calculator.spec.ts
@@ -1,35 +1,70 @@
+import { describe } from 'vitest';
 import {
   OverflowException,
   SantaTravelCalculator,
 } from '../src/travel/santaTravelCalculator';
 
 describe('SantaTravelCalculator', () => {
-  test.each([
+  const SimpleCase = [
     [1, 1],
     [2, 3],
     [5, 31],
-    [10, 1023],
-    [20, 1048575],
-    [30, 1073741823],
-  ])(
-    'should calculate the distance correctly for %d reindeers',
-    (numberOfReindeers, expectedDistance) => {
-      expect(
-        SantaTravelCalculator.calculateTotalDistanceRecursively(
-          numberOfReindeers,
-        ),
-      ).toBe(expectedDistance);
-    },
-  );
+    [10, 1_023],
+    [20, 1_048_575],
+    [30, 1_073_741_823],
+  ];
+  const LargeCase = [
+    [32, 4_294_967_295],
+    [50, 1_125_899_906_842_623],
+    // Just for fun, this is the largest number of reindeers we can have using number and without having Infinity as a result.
+    [1023, 8.98846567431158e307],
+  ];
 
-  test.each([[32], [50]])(
-    'should fail for numbers greater or equal to 32 (%d)',
-    (numberOfReindeers) => {
-      expect(() =>
-        SantaTravelCalculator.calculateTotalDistanceRecursively(
-          numberOfReindeers,
-        ),
-      ).toThrow(OverflowException);
-    },
-  );
+  describe('calculateTotalDistanceRecursively', () => {
+    test.each(SimpleCase)(
+      'should calculate the distance correctly for %d reindeers',
+      (numberOfReindeers, expectedDistance) => {
+        expect(
+          SantaTravelCalculator.calculateTotalDistanceRecursively(
+            numberOfReindeers,
+          ),
+        ).toBe(expectedDistance);
+      },
+    );
+
+    test.each(LargeCase)(
+      'should fail for numbers greater or equal to 32 (%d)',
+      (numberOfReindeers) => {
+        expect(() =>
+          SantaTravelCalculator.calculateTotalDistanceRecursively(
+            numberOfReindeers,
+          ),
+        ).toThrow(OverflowException);
+      },
+    );
+  });
+
+  describe('calculateTotalDistanceNotRecursively', () => {
+    test.each([...SimpleCase, ...LargeCase])(
+      'should calculate the distance correctly for %d reindeers',
+      (numberOfReindeers, expectedDistance) => {
+        expect(
+          SantaTravelCalculator.calculateTotalDistanceNotRecursively(
+            numberOfReindeers,
+          ),
+        ).toBe(expectedDistance);
+      },
+    );
+
+    test.each(LargeCase)(
+      'should not fail for numbers greater or equal to 32 (%d)',
+      (numberOfReindeers) => {
+        expect(() =>
+          SantaTravelCalculator.calculateTotalDistanceNotRecursively(
+            numberOfReindeers,
+          ),
+        ).not.toThrow(OverflowException);
+      },
+    );
+  });
 });
