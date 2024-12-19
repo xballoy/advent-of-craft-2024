2840dd9ead670245f21d986dab4c309ecef19163
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-19 14:44:36 -0500

    Prepare day18 and day19 (#21)

--- exercise/ts/day18/features/step_definitions/rockPapersScissorsSteps.ts
+++ exercise/ts/day18/features/step_definitions/rockPapersScissorsSteps.ts
@@ -1,20 +1,30 @@
-import {Given, Then, When} from "@cucumber/cucumber";
-import {Choice, Result, RockPaperScissors} from "../../src/rockPaperScissors";
-import * as assert from "assert";
+import assert from 'node:assert';
+import { Given, Then, When } from '@cucumber/cucumber';
+import {
+  type Choice,
+  type Result,
+  RockPaperScissors,
+} from '../../src/rockPaperScissors';
 
 let result: Result;
 let player1: Choice;
 let player2: Choice;
 
-Given(/^Player (\d+) chooses (.*)$/, function (player, choice) {
-    if (player === 1) player1 = choice;
-    else player2 = choice;
+Given(/^Player (\d+) chooses (.*)$/, (player, choice) => {
+  if (player === 1) player1 = choice;
+  else player2 = choice;
 });
 
-When(/^they play$/, function () {
-    result = RockPaperScissors.play(player1, player2)
+When(/^they play$/, () => {
+  result = RockPaperScissors.play(player1, player2);
 });
 
-Then(/^the result should be (.*) because (.*)$/, function (expectedWinner, expectedReason) {
-    assert.deepEqual(result, {winner: expectedWinner, reason: expectedReason});
-});
+Then(
+  /^the result should be (.*) because (.*)$/,
+  (expectedWinner, expectedReason) => {
+    assert.deepEqual(result, {
+      winner: expectedWinner,
+      reason: expectedReason,
+    });
+  },
+);
