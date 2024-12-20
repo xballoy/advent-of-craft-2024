b9b8b4033874ca0faaf973087ef73e494456b204
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-20 10:53:25 -0500

    Prepare day 20 (#24)

--- exercise/ts/day20/src/types.ts
+++ exercise/ts/day20/src/types.ts
@@ -1,30 +1,30 @@
 export class Reindeer {
-    constructor(
-        public id: string,
-        public name: string,
-        public color: ReindeerColor
-    ) {}
+  constructor(
+    public id: string,
+    public name: string,
+    public color: ReindeerColor,
+  ) {}
 }
 
 export enum ReindeerColor {
-    White = 0,
-    Black = 1,
-    Purple = 2
+  White = 0,
+  Black = 1,
+  Purple = 2,
 }
 
 export enum ReindeerErrorCode {
-    NotFound,
-    AlreadyExist
+  NotFound = 0,
+  AlreadyExist = 1,
 }
 
 export class ReindeerResult {
-    constructor(
-        public id: string,
-        public name: string,
-        public color: ReindeerColor
-    ) {}
+  constructor(
+    public id: string,
+    public name: string,
+    public color: ReindeerColor,
+  ) {}
 
-    public static from(reindeer: Reindeer): ReindeerResult {
-        return new ReindeerResult(reindeer.id, reindeer.name, reindeer.color);
-    }
+  public static from(reindeer: Reindeer): ReindeerResult {
+    return new ReindeerResult(reindeer.id, reindeer.name, reindeer.color);
+  }
 }
