10db18a20719739bd08bf0e75a8b42289c12f4b8
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-21 17:26:18 -0500

    Refactor: same features

--- exercise/ts/day21/src/tour/tourCalculator.ts
+++ exercise/ts/day21/src/tour/tourCalculator.ts
@@ -13,8 +13,8 @@ export type Step = {
 
 export class TourCalculator implements ITourCalculator {
   private steps: Step[];
-  private calculated: boolean = false;
-  private _deliveryTime: number = 0;
+  private calculated = false;
+  private _deliveryTime = 0;
 
   constructor(steps: Step[]) {
     this.steps = steps;
@@ -27,6 +27,7 @@ export class TourCalculator implements ITourCalculator {
   calculate(): Either.Either<string, string> {
     if (!this.steps || this.steps.length === 0) {
       return Either.left('No locations !!!');
+      // biome-ignore lint/style/noUselessElse: <explanation>
     } else {
       const result: string[] = [];
 
@@ -54,9 +55,9 @@ export class TourCalculator implements ITourCalculator {
 }
 
 export class NewTourCalculator implements ITourCalculator {
-  private steps: Step[];
-  private calculated: boolean = false;
-  private _deliveryTime: number = 0;
+  private readonly steps: Step[];
+  private calculated = false;
+  private _deliveryTime = 0;
 
   constructor(steps: Step[]) {
     this.steps = steps;
@@ -67,30 +68,34 @@ export class NewTourCalculator implements ITourCalculator {
   }
 
   calculate(): Either.Either<string, string> {
-    if (!this.steps || this.steps.length === 0) {
+    if (!this.steps.length) {
       return Either.left('No locations !!!');
-    } else {
-      const result: string[] = [];
-
-      for (const s of this.steps.sort((a, b) => (a.time > b.time ? 1 : -1))) {
-        if (!this.calculated) {
-          this._deliveryTime += s.deliveryTime;
-          result.push(this.fLine(s, this._deliveryTime));
-        }
-      }
-
-      const str = new Date(this._deliveryTime * 1000)
-        .toISOString()
-        .substring(11);
-      result.push(`Delivery time | ${str}`);
-      this.calculated = true;
-
-      return Either.right(result.join('\n'));
     }
+
+    const result: string[] = [];
+    if (!this.calculated) {
+      for (const step of this.getSortedSteps()) {
+        this._deliveryTime += step.deliveryTime;
+        result.push(this.formatStep(step));
+      }
+    }
+    this.calculated = true;
+
+    result.push(
+      `Delivery time | ${this.formatDeliveryTime(this._deliveryTime)}`,
+    );
+    return Either.right(result.join('\n'));
   }
 
-  private fLine(step: Step, x: number): string {
-    if (!step) throw new Error('Invalid operation');
+  private formatStep(step: Step): string {
     return `${step.time} : ${step.label} | ${step.deliveryTime} sec`;
+  }
+
+  private getSortedSteps(): Step[] {
+    return [...this.steps].sort((a, b) => (a.time > b.time ? 1 : -1));
+  }
+
+  private formatDeliveryTime(deliveryTime: number): string {
+    return new Date(deliveryTime * 1000).toISOString().substring(11);
   }
 }
