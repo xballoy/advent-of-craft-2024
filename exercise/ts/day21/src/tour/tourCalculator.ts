cfba4c1ef9ab00f48108305f7e7739bf61938d6e
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-22 10:32:47 -0500

    Day 21: Safely refactor the TourCalculator (#26)
    * Add characterization test
    
    * Refactor: same features

--- exercise/ts/day21/src/tour/tourCalculator.ts
+++ exercise/ts/day21/src/tour/tourCalculator.ts
@@ -1,48 +1,101 @@
-import * as E from 'fp-ts/Either';
+import { Either } from 'effect';
 
-export type Step = {
-    time: string;
-    label: string;
-    deliveryTime: number;
+type ITourCalculator = {
+  deliveryTime: number;
+  calculate: () => Either.Either<string, string>;
 };
 
-export class TourCalculator {
-    private steps: Step[];
-    private calculated: boolean = false;
-    private _deliveryTime: number = 0;
+export type Step = {
+  time: string;
+  label: string;
+  deliveryTime: number;
+};
 
-    constructor(steps: Step[]) {
-        this.steps = steps;
-    }
+export class TourCalculator implements ITourCalculator {
+  private steps: Step[];
+  private calculated = false;
+  private _deliveryTime = 0;
 
-    get deliveryTime(): number {
-        return this._deliveryTime;
-    }
+  constructor(steps: Step[]) {
+    this.steps = steps;
+  }
 
-    calculate(): E.Either<string, string> {
-        if (!this.steps || this.steps.length === 0) {
-            return E.left('No locations !!!');
-        } else {
-            const result: string[] = [];
+  get deliveryTime(): number {
+    return this._deliveryTime;
+  }
 
-            for (const s of this.steps
-                .sort((a, b) => (a.time > b.time ? 1 : -1))) {
-                if (!this.calculated) {
-                    this._deliveryTime += s.deliveryTime;
-                    result.push(this.fLine(s, this._deliveryTime));
-                }
-            }
+  calculate(): Either.Either<string, string> {
+    if (!this.steps || this.steps.length === 0) {
+      return Either.left('No locations !!!');
+      // biome-ignore lint/style/noUselessElse: <explanation>
+    } else {
+      const result: string[] = [];
 
-            const str = new Date(this._deliveryTime * 1000).toISOString().substring(11);
-            result.push(`Delivery time | ${str}`);
-            this.calculated = true;
-
-            return E.right(result.join('\n'));
+      for (const s of this.steps.sort((a, b) => (a.time > b.time ? 1 : -1))) {
+        if (!this.calculated) {
+          this._deliveryTime += s.deliveryTime;
+          result.push(this.fLine(s, this._deliveryTime));
         }
+      }
+
+      const str = new Date(this._deliveryTime * 1000)
+        .toISOString()
+        .substring(11);
+      result.push(`Delivery time | ${str}`);
+      this.calculated = true;
+
+      return Either.right(result.join('\n'));
+    }
+  }
+
+  private fLine(step: Step, x: number): string {
+    if (!step) throw new Error('Invalid operation');
+    return `${step.time} : ${step.label} | ${step.deliveryTime} sec`;
+  }
+}
+
+export class NewTourCalculator implements ITourCalculator {
+  private readonly steps: Step[];
+  private calculated = false;
+  private _deliveryTime = 0;
+
+  constructor(steps: Step[]) {
+    this.steps = steps;
+  }
+
+  get deliveryTime(): number {
+    return this._deliveryTime;
+  }
+
+  calculate(): Either.Either<string, string> {
+    if (!this.steps.length) {
+      return Either.left('No locations !!!');
     }
 
-    private fLine(step: Step, x: number): string {
-        if (!step) throw new Error('Invalid operation');
-        return `${step.time} : ${step.label} | ${step.deliveryTime} sec`;
+    const result: string[] = [];
+    if (!this.calculated) {
+      for (const step of this.getSortedSteps()) {
+        this._deliveryTime += step.deliveryTime;
+        result.push(this.formatStep(step));
+      }
     }
+    this.calculated = true;
+
+    result.push(
+      `Delivery time | ${this.formatDeliveryTime(this._deliveryTime)}`,
+    );
+    return Either.right(result.join('\n'));
+  }
+
+  private formatStep(step: Step): string {
+    return `${step.time} : ${step.label} | ${step.deliveryTime} sec`;
+  }
+
+  private getSortedSteps(): Step[] {
+    return [...this.steps].sort((a, b) => (a.time > b.time ? 1 : -1));
+  }
+
+  private formatDeliveryTime(deliveryTime: number): string {
+    return new Date(deliveryTime * 1000).toISOString().substring(11);
+  }
 }
