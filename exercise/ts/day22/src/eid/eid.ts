85488c14a855f6f7aba792e97dac65f59ef3d4fb
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-12-22 10:39:14 -0500

    Prepare day 22

--- exercise/ts/day22/src/eid/eid.ts
+++ exercise/ts/day22/src/eid/eid.ts
@@ -1,67 +1,69 @@
-import {Either, isLeft, left, right, chain} from 'fp-ts/Either';
-import {Sex} from "./sex";
-import {Year} from "./year";
-import {SerialNumber} from "./serialNumber";
-import {ParsingException} from "./parsingException";
-import {pipe} from "fp-ts/function";
+import { Either, pipe } from 'effect';
+import { ParsingException } from './parsingException';
+import { SerialNumber } from './serialNumber';
+import { Sex } from './sex';
+import { Year } from './year';
 
 export class EID {
-    constructor(
-        private readonly sex: Sex,
-        private readonly year: Year,
-        private readonly serialNumber: SerialNumber
-    ) {
-    }
+  constructor(
+    private readonly sex: Sex,
+    private readonly year: Year,
+    private readonly serialNumber: SerialNumber,
+  ) {}
 
-    static parse(potentialEID: string): Either<ParsingException, EID> {
+  static parse(potentialEID: string): Either.Either<EID, ParsingException> {
+    return pipe(
+      Sex.parse(potentialEID.substring(0, 1)),
+      Either.flatMap((sex) => {
         return pipe(
-            Sex.parse(potentialEID[0]),
-            chain(sex =>
-                pipe(
-                    Year.parse(potentialEID.substring(1, 3)),
-                    chain(year =>
-                        pipe(
-                            SerialNumber.parse(potentialEID.substring(3, 6)),
-                            chain(serialNumber => {
-                                const eid = new EID(sex, year, serialNumber);
-                                return eid.checkKey(potentialEID.substring(6))
-                                    ? right(eid)
-                                    : left(new ParsingException('Invalid key'));
-                            })
-                        )
-                    )
-                )
-            )
+          Year.parse(potentialEID.substring(1, 3)),
+          Either.flatMap((year) => {
+            return pipe(
+              SerialNumber.parse(potentialEID.substring(3, 6)),
+              Either.flatMap((serialNumber) => {
+                const eid = new EID(sex, year, serialNumber);
+                return eid.checkKey(potentialEID.substring(6))
+                  ? Either.right(eid)
+                  : Either.left(new ParsingException('Invalid key'));
+              }),
+            );
+          }),
         );
-    }
+      }),
+    );
+  }
 
-    private checkKey(potentialKey: string): boolean {
-        const key = parseInt(potentialKey);
-        return !isNaN(key) && this.key() === key;
-    }
+  private checkKey(potentialKey: string): boolean {
+    const key = Number.parseInt(potentialKey);
+    return !Number.isNaN(key) && this.key() === key;
+  }
 
-    private stringWithoutKey(): string {
-        return `${this.sex}${this.year}${this.serialNumber}`;
-    }
+  private stringWithoutKey(): string {
+    return `${this.sex}${this.year}${this.serialNumber}`;
+  }
 
-    private toLong(): number {
-        return parseInt(this.sex.toString() + this.year + this.serialNumber.toString());
-    }
+  private toLong(): number {
+    return Number.parseInt(
+      this.sex.toString() + this.year + this.serialNumber.toString(),
+    );
+  }
 
-    key(): number {
-        return 97 - (this.toLong() % 97);
-    }
+  key(): number {
+    return 97 - (this.toLong() % 97);
+  }
 
-    toString(): string {
-        return this.stringWithoutKey() + this.key().toString().padStart(2, '0');
-    }
+  toString(): string {
+    return this.stringWithoutKey() + this.key().toString().padStart(2, '0');
+  }
 
-    equals(other: EID): boolean {
-        if (!(other instanceof EID)) {
-            return false;
-        }
-        return this.sex === other.sex &&
-            this.year.equals(other.year) &&
-            this.serialNumber.equals(other.serialNumber);
+  equals(other: EID): boolean {
+    if (!(other instanceof EID)) {
+      return false;
     }
+    return (
+      this.sex === other.sex &&
+      this.year.equals(other.year) &&
+      this.serialNumber.equals(other.serialNumber)
+    );
+  }
 }
